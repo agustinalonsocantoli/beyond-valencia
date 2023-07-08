4da7a752c63edf0c90d24e7d47e28381ce1603f3
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-07-08 18:02:13 +0200

    descommnet some routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 // Components
 import { Home } from "./pages/Home";
-// import { Experiences } from "./pages/Experiences";
-// import { Daytrips } from "./pages/Daytrips";
-// import { Food } from "./pages/Food";
-// import { Details } from "./pages/Details";
+import { Experiences } from "./pages/Experiences";
+import { Daytrips } from "./pages/Daytrips";
+import { Food } from "./pages/Food";
+import { Details } from "./pages/Details";
 import { Options } from "./pages/Options";
 import { Lockers } from "./pages/Lockers";
 import { Bikes } from "./pages/Bikes";
@@ -17,11 +17,11 @@ function App() {
       <Router basename="/">
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="experiences/*" element={<Experiences />} />
+            <Route path="experiences/*" element={<Experiences />} />
             <Route path="daytrips/*" element={<Daytrips />} />
             <Route path="food/*" element={<Food />} />
             <Route path="aboutus/*" element={<div>About Us</div>} />
-            <Route path="details/*" element={<Details />} /> */}
+            <Route path="details/*" element={<Details />} />
             <Route path="more-services/*" element={<Options />} />
             <Route path="lockers/*" element={<Lockers />} />
             <Route path="bikes/*" element={<Bikes />} />
