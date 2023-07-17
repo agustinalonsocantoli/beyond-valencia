2118a1d4b1c863d19699fefde110447928966a6f
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-07-17 22:12:01 +0200

    test mui dont work styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,25 @@ import { Details } from "./pages/Details";
 import { Options } from "./pages/Options";
 import { Lockers } from "./pages/Lockers";
 import { Bikes } from "./pages/Bikes";
-import { StyledEngineProvider } from "@mui/material";
 
 function App() {
 
   return (
     <div className="app">
       <Router basename="/">
-        <StyledEngineProvider injectFirst>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="experiences/*" element={<Experiences />} />
-            <Route path="experiences/:slug" element={<Details title="Experiences" />} />
-            <Route path="daytrips/*" element={<Daytrips />} />
-            <Route path="daytrips/:slug" element={<Details title="DayTrips"/>} />
-            {/* <Route path="food/*" element={<Food />} /> */}
-            {/* <Route path="aboutus/*" element={<div>About Us</div>} /> */}
-            <Route path="more-services/*" element={<Options />} />
-            <Route path="lockers/*" element={<Lockers />} />
-            <Route path="bikes/*" element={<Bikes />} />
-            <Route path="*" element={<Navigate to={"/"} />} />
-          </Routes>
-        </StyledEngineProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="experiences/*" element={<Experiences />} />
+          <Route path="experiences/:slug" element={<Details title="Experiences" />} />
+          <Route path="daytrips/*" element={<Daytrips />} />
+          <Route path="daytrips/:slug" element={<Details title="DayTrips"/>} />
+          {/* <Route path="food/*" element={<Food />} /> */}
+          {/* <Route path="aboutus/*" element={<div>About Us</div>} /> */}
+          <Route path="more-services/*" element={<Options />} />
+          <Route path="lockers/*" element={<Lockers />} />
+          <Route path="bikes/*" element={<Bikes />} />
+          <Route path="*" element={<Navigate to={"/"} />} />
+        </Routes>
       </Router>
     </div>
   )
